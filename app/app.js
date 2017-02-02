c5e05c09c3bbf8aadaddd21feddf69c6d00eed17
Jed Cayetano <JedC@Magenic.com>
2017-02-03 02:55:34 +0800

    Added dashboard container page

--- app/app.js
+++ app/app.js
@@ -6,12 +6,14 @@ import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import Styles from '../public/css/style.css';
 import { Router, Route, hashHistory } from 'react-router'
 var PageMaster = require('./components/page-master.js');
+var Dashboard = require('./components/dashboard.js');
 var Kanban = require('./components/kanban.js');
 var TaskContainer = require('./components/taskcontainer.js');
 
 ReactDOM.render(
     <Router history={hashHistory}>
-        <Route path="/" component={PageMaster}>
+        <Route component={PageMaster}>
+            <Route path="/" component={Dashboard} />
             <Route path="/taskList" component={TaskContainer}/>
             <Route path="/kanban" component={Kanban}/>
         </Route>
